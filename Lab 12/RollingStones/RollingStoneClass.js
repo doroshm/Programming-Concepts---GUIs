b44720b0d0043191583aec74e6477f1b10917552
doroshm <115737819+doroshm@users.noreply.github.com>
2023-05-06 18:38:16 -0400

    Add files via upload

---
+++ Lab 12/RollingStones/RollingStoneClass.js
@@ -0,0 +1,12 @@
+class RollingStone {
+    constructor(rank, title, performer, writer, producer, released, chartHistory, imgRecord){
+        this.rank = rank;
+        this.title = title;
+        this.performer = performer;
+        this.writer = writer;
+        this.producer = producer;
+        this.released = released;
+        this.chartHistory = chartHistory;
+        this.imgRecord = imgRecord;
+    }
+}
---
+++ Lab 13/TedTalk/ted.js
@@ -0,0 +1,11 @@
+class classTED {
+    constructor(tedRank, tedTitle, tedAuthor, tedTime, tedViews, tedDesc, tedYouTube){
+        this.tedRank = tedRank;
+        this.tedTitle = tedTitle;
+        this.tedAuthor = tedAuthor;
+        this.tedTime = tedTime;
+        this.tedViews = tedViews;
+        this.tedDesc = tedDesc;
+        this.tedYouTube = tedYouTube;
+    }
+}
